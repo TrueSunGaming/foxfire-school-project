8630cb2ef4eca995e5c6373047494c8a735778c1
TrueSunGaming <79117622+TrueSunGaming@users.noreply.github.com>
2024-09-20 20:31:58 -0400

    feat: add globals and age var

--- components/player/player.gd
+++ components/player/player.gd
@@ -2,7 +2,12 @@ class_name Player extends CharacterBody2D
 
 @export var movement_speed: float
 
+var age := 0
+
 func _physics_process(delta: float) -> void:
 	velocity = Input.get_vector("left", "right", "up", "down") * movement_speed
 	
 	move_and_slide()
+
+func _ready() -> void:
+	refs.player = self
